f78c83385cc882570963741a393e133c1a26ef60
CaddyDz <caddydz4@gmail.com>
2020-09-08 22:00:17 +0100

    Login with phone number
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,12 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ];
+	return [
+		'name' => $faker->name,
+		'phone' => $faker->regexify('/^(0)(5|6|7)(4|5|6|7)[0-9]{7}$/'),
+		'email' => $faker->unique()->safeEmail,
+		'email_verified_at' => now(),
+		'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+		'remember_token' => Str::random(10),
+	];
 });
