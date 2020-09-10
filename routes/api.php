51bf59bc5b16a2104a562a082e21b07eccd7d21c
CaddyDz <caddydz4@gmail.com>
2020-09-10 01:41:17 +0100

    Removed api login route
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
 	return $request->user();
 });
-Route::post('/login', function () {
-	if (Auth::attempt(request()->all())) {
-		return response([
-			'status' => 'success'
-		], 200);
-	}
-	return response([
-		'status' => 'failed'
-	], 401);
-});
